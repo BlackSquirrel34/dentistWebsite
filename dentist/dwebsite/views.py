f26dbd9ed6c343dbe1a048120cbe4d3d5b7c8153
Edith <edith-friedgund-johanna.foerster@stud.uni-bamberg.de>
2024-10-02 18:46:46 +0200

    contact form set up

--- dentist/dwebsite/views.py
+++ dentist/dwebsite/views.py
@@ -5,4 +5,13 @@ def home(request):
     return render(request, 'dwebsite/home.html', {})
 
 def contact(request):
-    return render(request, 'dwebsite/contact.html', {})
+    if request.method == "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        return render(request, 'dwebsite/contact.html', {})
+
+    else:
+        return render(request, 'dwebsite/contact.html', {})
